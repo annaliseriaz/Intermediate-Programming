6a823ef1625d79de927f251b359f0f26f9ac5092
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 13:13:35 +0800

    Create Objects.java

---
+++ Objects.java
@@ -0,0 +1,16 @@
+public class Objects {
+	
+	public void fullThrottle() {
+		System.out.println("The car is going as fast as it can!");
+	}
+	
+	public void speed (int maxSpeed) {
+		System.out.println("Max speed is: " + maxSpeed);
+	}
+	
+	public static void main(String[] args) {
+		Objects myCar = new Objects();
+		myCar.fullThrottle();
+		myCar.speed(200); 
+	}
+} 
