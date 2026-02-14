57b5368efb8b062be0057bd7380a79956b618214
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 13:22:07 +0800

    Create LastItemMidtermExam.java

---
+++ LastItemMidtermExam.java
@@ -0,0 +1,9 @@
+package exam;
+
+public class LastItemMidtermExam {
+
+	public static void main(String[] args) {
+		String statement = "Midterm Exam";
+		System.out.println(statement.substring(3, 7));
+	}
+}
