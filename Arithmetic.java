67dfb8f91226ade8b950925e38e237c67d6749d8
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 13:35:32 +0800

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,21 @@
+package math;
+
+public class Arithmetic {
+		public int addition(int num1, int num2) {
+			int sum;
+			sum = num1 + num2;
+			return sum;
+		}
+		
+		public int subtract(int num1, int num2) {
+			int diff;
+			diff = num1 - num2;
+			return diff;
+		}
+		
+		public int multiply(int num1, int num2) {
+			int mult;
+			mult = num1 * num2;
+			return mult;
+		}
+	}
