2ee70d5aaeacc193fc452ada57ab81a788cb95e0
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 13:16:54 +0800

    Create Exam2.java

---
+++ Exam2.java
@@ -0,0 +1,10 @@
+package class_object_pck;
+
+public class Exam2 {
+
+	public static void main(String[] args) {
+		MyRecur mr = new MyRecur();
+		int result = mr.myValue(5);
+		System.out.println(result);
+	}
+}
