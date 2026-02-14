522c9b61d13d93d4326c4a2a40f26911b74aafaf
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 13:25:20 +0800

    Create Constructors.java

---
+++ Constructors.java
@@ -0,0 +1,14 @@
+package java_constructors;
+
+public class Constructors {
+	int x;
+	
+	public Constructors() {
+		x = 5;
+	}
+	
+	public static void main(String[] args) {
+		Constructors main = new Constructors();
+		System.out.println(main.x); 
+	}
+}
